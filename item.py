970f66e53cb6af0a81a788e200ddfa230d76b56c
Huzaifa <huzaifa@devs-MacBook-Air.local>
2025-08-21 06:00:56 -0700

    ft update: enhanced in and out model for item

--- item.py
+++ item.py
@@ -2,9 +2,16 @@ from pydantic import BaseModel, confloat
 
 
 
-class Item(BaseModel):
+class Item_in(BaseModel):
+    title : str
+    description : str = "No description Added yet"
+    price : confloat(ge = 0.0)
+
+
+class Item_out(BaseModel):
     id : int
     title : str
     description : str = "No description Added yet"
     price : confloat(ge = 0.0)
-    
+
+
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, File, UploadFile
 from fastapi.security import OAuth2PasswordBearer
-from .item import Item
+from .item import Item_in,Item_out
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+fake_db = []
+counter = 0
 
 @app.get("/items/")
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
@@ -17,6 +19,12 @@ async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
 async def upload(file: UploadFile = File(...)):
     return {"filename": file.filename}
 
-@app.post("/create_item/")
-async def create_item(item : Item):
-    return item
+@app.post("/create_item/",response_model=Item_out)
+async def create_item(item : Item_in):
+    global counter
+    counter += 1 
+
+    item_out = Item_out(id=counter, **item.model_dump())
+    fake_db.append(item_out)
+
+    return item_out
